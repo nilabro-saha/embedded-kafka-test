caa6f97160add069365fa8433163af5d5a48d932
Nilabro Saha <nilabrosaha2001@gmail.com>
2022-12-09 23:07:03 +0530

    First commit

---
+++ src/main/java/com/example/embeddedkafkatest/EmbeddedKafkaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.embeddedkafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedKafkaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedKafkaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/embeddedkafkatest/EmbeddedKafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.embeddedkafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedKafkaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
